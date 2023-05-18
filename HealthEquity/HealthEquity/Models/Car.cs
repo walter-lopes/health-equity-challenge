2b5798d18b09982ee88401d195d9dc553b15afae
waltercardoso <walterc@kennards.com.au>
2023-05-18 14:40:04 -0300

    fixed update

--- HealthEquity/HealthEquity/Models/Car.cs
+++ HealthEquity/HealthEquity/Models/Car.cs
@@ -9,4 +9,14 @@ public class Car
     public int Doors { get; set; }
     public string Color { get; set; }
     public decimal Price { get; set; }
+
+    public void Update(string make, string model, int year, int doors, string color, decimal price)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        Doors = doors;
+        Color = color;
+        Price = price;
+    }
 }
